7f2bea2a3fb03415ad00490d0af4a85bd4f00cd0
Falgun Bhalsod <falgun@yellowpanther.co.uk>
2025-02-07 12:02:08 +0530

    Test lambda3

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 // index.js
 exports.handler = async (event) => {
-    const functionName = event.queryStringParameters && event.queryStringParameters.function;
-  
-    switch (functionName) {
-      case 'hello1':
-        return hello1();
-      case 'hello2':
-        return hello2();
-      case 'hello3':
-        return hello3();
-      default:
-        return {
-          statusCode: 400,
-          body: JSON.stringify({ message: 'Invalid function name' }),
-        };
-    }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from Lambda!',
+    }),
   };
-  
-  function hello1() {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Hello from Hello1!' }),
-    };
-  }
-  
-  function hello2() {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Hello from Hello2!' }),
-    };
-  }
-  
-  function hello3() {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Hello from Hello3!' }),
-    };
-  }
+  return response;
+};
